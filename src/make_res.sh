82cf0299c63b7927992f9ae3c154c57b5ce865de
mvlassis <manos.vlassis@canonical.com>
2024-12-17 01:07:04 +0200

    Add shiny Pokemon

--- src/make_res.sh
+++ src/make_res.sh
@@ -1,15 +1,23 @@
+set -e
+
 script_path="$(readlink -f "${0}")"
 src_path="$(dirname "${script_path}")"
 root_path="$(dirname "${src_path}")"
 . "${src_path}/utils.sh"
-check_dependencies svn curl mogrify
+check_dependencies git curl mogrify
 
 # Use subversion to download only the directory containing the platinum sprites. Use export to exclude .svn.
 printf "Downloading directory with sprites\n"
-svn export --force -q https://github.com/PokeAPI/sprites/trunk/sprites/pokemon/versions/generation-iv/platinum "${root_path}/res"
+
+git clone -n --depth=1 --filter=tree:0 https://github.com/PokeAPI/sprites
+git -C sprites sparse-checkout set --no-cone /sprites/pokemon/versions/generation-iv/platinum
+git -C sprites checkout
+
+mv ./sprites/sprites/pokemon/versions/generation-iv/platinum/* "${root_path}/res/"
+rm -rf ./sprites
 
 # Remove useless resources
-rm -rf "${root_path}/res/back" "${root_path}/res/shiny" "${root_path}/res/female"
+rm -rf "${root_path}/res/back" "${root_path}/res/female" "${root_path}/res/shiny/female"
 
 # Keep only one Unown form
 rm "${root_path}/res/201-"*
@@ -36,11 +44,38 @@ rm "${root_path}/res/492-"*
 # Keep only one Arceus form
 rm "${root_path}/res/493-"*
 
+# And the same for shiny forms
+# Keep only one Unown form
+rm "${root_path}/res/shiny/201-"*
+# Keep only one Castform form
+rm "${root_path}/res/shiny/351-"*
+# Keep only one Deoxys form
+rm "${root_path}/res/shiny/386-"*
+# Keep only one Burmy form
+rm "${root_path}/res/shiny/412-"*
+# Keep only one Wormadam form
+rm "${root_path}/res/shiny/413-"*
+# Keep only one Cherrim form
+rm "${root_path}/res/shiny/421-"*
+# Keep only one Shellos form
+rm "${root_path}/res/shiny/422-"*
+# Keep only one Gastrodon form
+rm "${root_path}/res/shiny/423-"*
+# Keep only one Rotom form
+rm "${root_path}/res/shiny/479-"*
+# Keep only one Giratina form
+rm "${root_path}/res/shiny/487-"*
+# Keep only one Shaymin form
+rm "${root_path}/res/shiny/492-"*
+# Keep only one Arceus form
+rm "${root_path}/res/shiny/493-"*
+
 printf "Calling PokeAPI to get names\n"
 for id in {1..493}; do
     printf "${id}/493\n"
     name=$(curl -LsS "https://pokeapi.co/api/v2/pokemon/${id}" | python3 -c "import json,sys; obj=json.load(sys.stdin); print(obj['name'])")
     mv "${root_path}/res/${id}.png" "${root_path}/res/${id}-${name^}.png"
+	mv "${root_path}/res/shiny/${id}.png" "${root_path}/res/shiny/${id}-${name^}.png"
 done
 
 # Hardcoded renames
@@ -49,5 +84,12 @@ mv "${root_path}/res/413-Wormadam-plant.png" "${root_path}/res/413-Wormadam.png"
 mv "${root_path}/res/487-Giratina-altered.png" "${root_path}/res/487-Giratina.png"
 mv "${root_path}/res/492-Shaymin-land.png" "${root_path}/res/492-Shaymin.png"
 
+# Hardcoded renames
+mv "${root_path}/res/shiny/386-Deoxys-normal.png" "${root_path}/res/shiny/386-Deoxys.png"
+mv "${root_path}/res/shiny/413-Wormadam-plant.png" "${root_path}/res/shiny/413-Wormadam.png"
+mv "${root_path}/res/shiny/487-Giratina-altered.png" "${root_path}/res/shiny/487-Giratina.png"
+mv "${root_path}/res/shiny/492-Shaymin-land.png" "${root_path}/res/shiny/492-Shaymin.png"
+
 # Trim transparency
 mogrify -trim "${root_path}/res/*"
+mogrify -trim "${root_path}/res/shiny/*"
--- src/pokecat.sh
+++ src/pokecat.sh
@@ -4,6 +4,42 @@ root_path="$(dirname "${src_path}")"
 . "${src_path}/utils.sh"
 check_dependencies catimg
 
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hv] [-s/--shiny]...
+Print a Pokemon sprite from a res folder
+    -h          display this help and exit
+    -s/--shiny  set the Pokemon to be shiny
+EOF
+}
+
+# One Pokemon of this {SHINY_CHANCE_DENOMINATOR} will be shiny
+SHINY_CHANCE_DENOMINATOR=512
+
+is_shiny=0
+# Loop to receive arguments
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        -s|--shiny)
+            is_shiny=1 # The Pokemon will be shiny
+            ;;
+        --)              # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)
+            break
+    esac
+    shift
+done
+
 # Check that ./res is indeed a directory
 if [ ! -d "${root_path}/res" ]; then
     printf "\e[31m${script_path}: ${root_path}/res is not a directory. Run ${src_path}/make_res.sh.\e[0m\n" >& 2
@@ -14,6 +50,19 @@ fi
 # head -n 1 reads only the first line and then exits. When sort tries to write data to the closed pipe, the system raises a "Broken pipe" signal (SIGPIPE)
 pokemon="$(ls "${root_path}/res" | sort -R | awk "NR==1" | xargs basename -s ".png")"
 
+# Get a random number, and if it's 1, then choose a shiny
+SHINY_NUMBER="$(shuf -i 1-${SHINY_CHANCE_DENOMINATOR} -n 1)"
+if [[ "${SHINY_NUMBER}" -eq 1 ]]; then
+	is_shiny=1
+fi
+
 # Show the pokemon. Replace catimg with the terminal image viewer of your choice
-catimg "${root_path}/res/${pokemon}.png"
-printf "\e[1m${pokemon}\e[0m\n"
+if [[ "${is_shiny}" -eq 1 ]]; then
+	catimg "${root_path}/res/shiny/${pokemon}.png"
+	printf "\e[1;33m${pokemon}\e[0m, and it's shiny!\n"
+else
+	catimg "${root_path}/res/${pokemon}.png"
+	printf "\e[1m${pokemon}\e[0m\n"
+fi
+
+
